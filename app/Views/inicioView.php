b4c9869822721710dbf27a9e7d47330e9642d35c
Lean998 <leandroaguero83@gmail.com>
2025-05-08 16:02:17 -0300

    Update inicioView.php
    Modals con los formularios, contenedor para mostrar mensajes de error o exito y el script para que este ultimo funcione.

--- app/Views/inicioView.php
+++ app/Views/inicioView.php
@@ -30,7 +30,10 @@
             </ul>
         </div>
     </header>
-    <section class="d-flex col-12 flex-column align-items-center my-3">
+    <div class="container py-3">
+        <div id="mensaje-success" class="alert d-none" role="alert"></div>
+    </div>
+    <section class="d-flex col-12 flex-column align-items-center mb-3">
         <div class="col-10 d-flex flex-column align-items-center">
             <div class="d-flex align-items-end justify-content-center">
                 <?php if(isset($mascota_amos_list)){?>
@@ -57,7 +60,7 @@
             </div>
             <div class="col-12 d-flex flex-column align-items-center">
                 <div class="col-12 tabla d-flex align-items-center justify-content-center">
-                    <?php if(isset($table)){echo "<div class='d-flex align-content-start'>"; if(isset($tipoTabla)){echo "<div class='agregarButton'><button class='btn p-1 m-2'>"; switch($tipoTabla){ case "Mascotas": echo "Agregar Mascota";break; case "Amos": echo "Agregar Amo";break; case "Veterinarios": echo "Agregar Veterinario";break; case "AmoMascotas": echo "Agregar Relacion Amo-Mascota";break; case "MascotaAmos": echo "Agregar Relacion Mascota-Amo";} echo "</button></div>";} echo $table."</div>";}else{?> 
+                    <?php if(isset($table)){echo "<div class='d-flex flex-column align-content-start'>"; if(isset($tipoTabla)){echo "<div class='agregarButton'><button class='btn p-1 m-2' data-bs-toggle='modal' data-bs-target='#modalAgregar$tipoTabla'>"; switch($tipoTabla){ case "Mascotas": echo "Agregar Mascota";break; case "Amos": echo "Agregar Amo";break; case "Veterinarios": echo "Agregar Veterinario";break; case "AmoMascotas": echo "Agregar Relacion Amo-Mascota";break; case "MascotaAmos": echo "Agregar Relacion Mascota-Amo";} echo "</button></div>";} echo $table."</div>";}else{?> 
                         <div class="inicio col-12">
                             <div class="d-flex flex-column justify-content-center align-items-center">
                                 <img class="img-fluid" src="<?= substr(base_url(),0,-17)?>Plantilla/imgs/InicioLogo.png" alt="Logo">
@@ -68,8 +71,113 @@
             </div>
         </div>
     </section>
+    <!-- MODAL PARA EL ALTA DE MASCOTAS !-->
+    <div class="modal fade" id="modalAgregarMascotas" tabindex="-1" aria-labelledby="modalAgregarMascotasLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalAgregarMascotasLabel">Agregar mascota</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+                </div>
+                <div class="modal-body">    
+                <form action="<?=base_url()?>inicio/alta_mascotas " method="post">
+                        <div class="row mb-3">
+                            <div class="col-12 mb-3">
+                                <label for="nombreMascota" class="form-label">Nombre</label>
+                                <input type="text"class="form-control <?= session('errors.nombreMascota') ? 'is-invalid' : '' ?>" value="<?= old('nombreMascota') ?>" id="nombreMascota" name="nombreMascota" >
+                                <div class="invalid-feedback">
+                                    <?= session('errors.nombreMascota') ?? '' ?>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <label for="especieMascota" class="form-label">Especie</label>
+                                <input type="text" class="form-control <?= session('errors.especieMascota') ? 'is-invalid' : '' ?>" value="<?= old('especieMascota') ?>" id="especieMascota" name="especieMascota" >
+                                <div class="invalid-feedback">
+                                    <?= session('errors.especieMascota') ?? '' ?>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-12 mb-3">
+                                <label for="razaMascota" class="form-label">Raza</label>
+                                <input type="text" class="form-control <?= session('errors.razaMascota') ? 'is-invalid' : '' ?>" value="<?= old('razaMascota') ?>" id="razaMascota" name="razaMascota" >
+                                <div class="invalid-feedback">
+                                    <?= session('errors.razaMascota') ?? '' ?>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <label for="edadMascota" class="form-label">Edad</label>
+                                <input type="number" class="form-control <?= session('errors.edadMascota') ? 'is-invalid' : '' ?>" value="<?= old('edadMascota') ?>" id="edadMascota" name="edadMascota" min="0" >
+                                <div class="invalid-feedback">
+                                    <?= session('errors.edadMascota') ?? '' ?>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="d-grid d-md-flex justify-content-md-end">
+                        <button type="submit" class="btn btn-primary">Guardar Mascota</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- MODAL PARA EL ALTA DE AMOS !-->
+    <div class="modal fade" id="modalAgregarAmos" tabindex="-1" aria-labelledby="modalAgregarAmosLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalAgregarAmosLabel">Agregar mascota</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+                </div>
+                <div class="modal-body">    
+                <form action="<?=base_url()?>inicio/alta_amos " method="post">
+                        <div class="row mb-3">
+                            <div class="col-12 mb-3">
+                                <label for="nombreAmo" class="form-label">Nombre</label>
+                                <input type="text"class="form-control <?= session('errors.nombreAmo') ? 'is-invalid' : '' ?>" value="<?= old('nombreAmo') ?>" id="nombreAmo" name="nombreAmo" >
+                                <div class="invalid-feedback">
+                                    <?= session('errors.nombreAmo') ?? '' ?>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <label for="apellidoAmo" class="form-label">Apellido</label>
+                                <input type="text" class="form-control <?= session('errors.apellidoAmo') ? 'is-invalid' : '' ?>" value="<?= old('apellidoAmo') ?>" id="apellidoAmo" name="apellidoAmo" >
+                                <div class="invalid-feedback">
+                                    <?= session('errors.apellidoAmo') ?? '' ?>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-12 mb-3">
+                                <label for="telefonoAmo" class="form-label">Telefono</label>
+                                <input type="text" class="form-control <?= session('errors.telefonoAmo') ? 'is-invalid' : '' ?>" value="<?= old('telefonoAmo') ?>" id="telefonoAmo" name="telefonoAmo" >
+                                <div class="invalid-feedback">
+                                    <?= session('errors.telefonoAmo') ?? '' ?>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="d-grid d-md-flex justify-content-md-end">
+                        <button type="submit" class="btn btn-primary">Guardar Mascota</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>
-
+<script src="<?= substr(base_url(),0,-17) ?>Plantilla/scripts/mensajes.js"></script>
+<script>
+        <?php if (session('success')): ?>
+            mostrarMensaje('mensaje-success', <?= json_encode(session('success')) ?>, 'success');
+        <?php elseif(session('error')): ?>
+            mostrarMensaje('mensaje-success', <?= json_encode(session('error')) ?>, 'danger');
+        <?php endif ?>
+    </script>
 </html>
