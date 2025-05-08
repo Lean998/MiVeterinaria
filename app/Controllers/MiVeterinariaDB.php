42bf2d0be1f3ca4a4041d8845374dd1ab7cae01b
Javier Ochoa <118292286+Javierochoa159@users.noreply.github.com>
2025-05-08 20:25:21 -0300

    url

--- app/Controllers/MiVeterinariaDB.php
+++ app/Controllers/MiVeterinariaDB.php
@@ -5,6 +5,6 @@ use App\Models\MiVeterinariaDBModel;
 class MiVeterinariaDB extends BaseController
 {
     public function index(){
-        if(new MiVeterinariaDBModel())redirect()->to(base_url()."algo/");else log_message('debug', 'DB no Creada');
+        if(new MiVeterinariaDBModel())redirect()->to(base_url()."inicio");else log_message('debug', 'DB no Creada');
     }
 }
